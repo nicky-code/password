b62372adaa796cd51814d6a2d69991a712cf2d6b
nicky-code <aline.nicole7@gmail.com>
2019-09-05 22:32:13 +0200

    created the constructor and pass in 4 arguments in User class.

--- user.py
+++ user.py
@@ -3,3 +3,15 @@ class User:
     Class that generates new instances of users
     """
     
+    
+def _init_(self,username,email,password):
+    
+    '''
+    _init_ method that helps define properties for our objects.
+    
+    Args:
+        username: New user username.
+        email: New user email address.
+        password: New user password.
+    '''
+       
