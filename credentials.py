3c07fc46c7fcd94175cee9e8064051f6719c36e1
nicky-code <aline.nicole7@gmail.com>
2019-09-06 09:08:35 +0200

    created the constructor and pass in 6 arguments.

---
+++ credentials.py
@@ -0,0 +1,19 @@
+class Credentials:
+    """
+    Class that generates new instances of credentials
+    """
+    
+    
+def _init_(self,accountName,siteName,username,email,password):
+    
+    '''
+    _init_ method that helps define properties for our objects.
+    
+    Args:
+        accountName: New user accountName.
+        siteName: New user siteName you want to use.
+        username: New user username.
+        email: New user email address.
+        password: New user password.
+    '''
+       
