ea5d35a8f260aab6e4949acf0171205f0941bc53
nicky-code <aline.nicole7@gmail.com>
2019-09-06 11:32:54 +0200

    first test for both classes User and Credentials.

--- test.py
+++ test.py
@@ -11,6 +11,24 @@ class TestUser(unittest.TestCase):
         unittest.TestCase: TestCase class that helps in creating test cases.
     '''
 
+    #Items up here....
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_user = User("Nicky","aline.nicole7@gmail.com","virgo7") #create user object
+
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_user.username,"Nicky")
+        self.assertEqual(self.new_user.email,"aline.nicole7@gmail.com")
+        self.assertEqual(self.new_user.password,"virgo7")
+
+if __name__ == '__main__':
+    unittest.main()
 
 
 class TestCredentials(unittest.TestCase):
@@ -21,4 +39,25 @@ class TestCredentials(unittest.TestCase):
     Args:
         unittest.TestCase: TestCase class that helps in creating test cases
     '''
-    
+
+    #Items up here....
+
+    def setUp(self):
+        '''
+        Set up method to ru before each test cases.
+        '''
+        self.new_credentials = Credentials("Nicole","Facebook","Nicky","aline.nicole7@gmail.com","virgo7") #create credentials object
+
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_credentials.accountName,"Nicole")
+        self.assertEqual(self.new_credentials.siteName,"Facebook")
+        self.assertEqual(self.new_credentials.username,"Nicky")
+        self.assertEqual(self.new_credentials.email,"aline.nicole7@gmail.com")
+        self.assertEqual(self.new_credentials.password,"virgo7")
+
+if __name__ == '__main__':
+    unittest.main()
